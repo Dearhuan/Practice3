dd8e0f81a297ef6f8be744341041b075d7e862aa
dearhuan <clearhuan@qq.com>
2019-10-21 22:49:06 +0800

    vue-router init

---
+++ Vue/day6/project/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'this is a route test'
+    }
+  }
+}
+</script>
