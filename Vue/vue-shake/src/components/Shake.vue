a2c15ac566ecaf0c2b2ec65e0cac5766aa11fb8f
Hello <clearhuan@qq.com>
2019-11-16 14:48:03 +0800

    add

---
+++ Vue/vue-shake/src/components/Shake.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h4>手机摇一摇</h4>
+    <div>
+      <audio src="../assets/5018.mp3" ref="shakeAudio">
+        您的浏览器不支持audio标签
+      </audio>
+    </div>
+  </div>
+</template>
+
+<script>
+import Shake from 'shake.js'
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  mounted() {
+    let myShakeEvent = new Shake({
+      threshold: 20,
+      timeout: 1200
+    })
+    myShakeEvent.start();
+    window.addEventListener('shake',this.shakeEventDidOccur,false)
+  },
+  methods: {
+    shakeEventDidOccur(){
+      let audio = this.$refs.shakeAudio;
+      if(window.navigator.vibrate){
+        navigator.vibrate(500)
+      }
+      audio.play()
+    }
+  },
+}
+</script>
--- Vue/vue-shake/src/router/index.js
+++ Vue/vue-shake/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Shake from '@/components/Shake'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/Hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+     {
+      path: '/',
+      name: 'Shake',
+      component: Shake
     }
   ]
 })
